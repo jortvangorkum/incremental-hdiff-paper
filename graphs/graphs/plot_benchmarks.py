2f00a67763393904b1f6591c324fe568c71cd625
Jort van Gorkum <jortvangorkum@gmail.com>
2022-01-25 17:21:19 +0100

    Added background and updated plot

--- graphs/graphs/plot_benchmarks.py
+++ graphs/graphs/plot_benchmarks.py
@@ -1,5 +1,6 @@
 from functools import reduce
 import seaborn as sns
+import matplotlib.ticker as tck
 import matplotlib.pyplot as plt
 import pandas as pd
 from typing import List
@@ -17,8 +18,8 @@ def split_amount_into_column(df: pd.DataFrame):
     df[["Action", "Amount"]] = df["Name"].str.split('/', expand=True)
     return df
 
-def plot_benchmark(df: pd.DataFrame, xlabel=None, ylabel=None, legend_title=None, legend_labels=[]):
-    ax = sns.barplot(x="Amount", y="Mean", hue="Action", data=df, palette="pastel")
+def plot_benchmark(df: pd.DataFrame, actions=[], xlabel=None, ylabel=None, legend_title=None, legend_labels=[]):
+    ax = sns.barplot(x="Amount", y="Mean", hue="Action", data=df, palette="pastel", hue_order=actions)
     ax.set_yscale('log')
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
@@ -27,19 +28,65 @@ def plot_benchmark(df: pd.DataFrame, xlabel=None, ylabel=None, legend_title=None
     for i in range(len(legend_labels)):
         leg.get_texts()[i].set_text(legend_labels[i])
 
+def show_plot_benchmark(
+    df: pd.DataFrame, 
+    actions: List[str], 
+    xlabel: str, 
+    ylabel: str, 
+    legend_title: str, 
+    legend_labels: List[str]
+):
+    actions_df = get_actions(df, *actions)
+    plot_benchmark(actions_df, actions, xlabel, ylabel, legend_title, legend_labels)
+    plt.show()
+    plt.clf()
+
+def save_plot_benchmark(
+    file_path: str,
+    df: pd.DataFrame, 
+    actions: List[str], 
+    xlabel: str, 
+    ylabel: str, 
+    legend_title: str, 
+    legend_labels: List[str]
+):
+    actions_df = get_actions(df, *actions)
+    print(actions_df)
+    plot_benchmark(actions_df, actions, xlabel, ylabel, legend_title, legend_labels)
+    plt.savefig(f"{IMAGES_PATH}/{file_path}.pdf")
+
 if __name__ == "__main__":
-    benchmarks_df = pd.read_csv('./data/new_generate_benchmarks.csv')
+    benchmarks_df = pd.read_csv('./data/benchmarks.csv')
 
     split_df = split_amount_into_column(benchmarks_df)
 
-    df_fib_fib_map = get_actions(split_df, "Generate (Fib, Map)", "Generate (Fib, Map) with Map")
-    df_fib_map_change = get_actions(split_df, "Generate (Fib, Map) with Map", "Generate (Fib, Map) with Map Single Change")
+    split_df
+    print(split_df)
 
-    plot_benchmark(df_fib_fib_map, "The Amount of Nodes (2n + 1)", "The Mean Execution Time in Seconds", legend_title="Compute Fibonacci and Map", legend_labels=["Without Map", "With Map"])
-    plt.show()
-    plt.clf()
+    save_plot_benchmark(
+        "plot_generate_result_benchmark",
+        split_df,
+        ["Generate Result", "Generate (Result, Map)", "Generate (Result, Map) with Map"],
+        xlabel="The Amount of Nodes (2n + 1)",
+        ylabel="The Mean Execution Time in Seconds",
+        legend_title="Compute Result",
+        legend_labels=["Only Sum", "Sum and Map", "Sum and Map with Map"]
+    )
 
-    plot_benchmark(df_fib_map_change, "The Amount of Nodes (2n + 1)", "The Mean Execution Time in Seconds", legend_title="Compute Fibonacci and Map with Map", legend_labels=["Zero Changes", "Single Change"])
-    plt.show()
-    plt.clf()
-    
+    # show_plot_benchmark(
+    #     split_df,
+    #     ["Generate (Fib, Map)", "Generate (Fib, Map) with Map"],
+    #     xlabel="The Amount of Nodes (2n + 1)",
+    #     ylabel="The Mean Execution Time in Seconds",
+    #     legend_title="Compute Fibonacci and Map",
+    #     legend_labels=["Without Map", "With Map"]
+    # )
+
+    # show_plot_benchmark(
+    #     split_df,
+    #     ["Generate (Fib, Map) with Map", "Generate (Fib, Map) with Map Single Change"],
+    #     xlabel="The Amount of Nodes (2n + 1)",
+    #     ylabel="The Mean Execution Time in Seconds",
+    #     legend_title="Compute Fibonacci and Map with Map", 
+    #     legend_labels=["Zero Changes", "Single Change"]
+    # )
