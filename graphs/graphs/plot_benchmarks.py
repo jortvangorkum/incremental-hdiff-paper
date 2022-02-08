f818e176a4670e7b2f65658535dc958c8038ceb7
Jort van Gorkum <jortvangorkum@gmail.com>
2022-02-08 15:21:59 +0100

    Got here

--- graphs/graphs/plot_benchmarks.py
+++ graphs/graphs/plot_benchmarks.py
@@ -1,17 +1,18 @@
 from functools import reduce
 import seaborn as sns
-import matplotlib.ticker as tck
 import matplotlib.pyplot as plt
+from tabulate import tabulate
 import pandas as pd
 from typing import List
 
 IMAGES_PATH = '../images'
+TABLES_PATH = '../tables'
 
 def get_actions(df: pd.DataFrame, *args: List[str]):
     actions = list(map(lambda x: df["Action"] == x, args))
     combined_actions = reduce(lambda x, y: x | y, actions)
     df = df.loc[combined_actions]
-    df = df.sort_values("Amount")
+    df = df.sort_values(["Amount", "Action"])
     return df
 
 def split_amount_into_column(df: pd.DataFrame):
@@ -51,17 +52,35 @@ def save_plot_benchmark(
     legend_labels: List[str]
 ):
     actions_df = get_actions(df, *actions)
-    print(actions_df)
     plot_benchmark(actions_df, actions, xlabel, ylabel, legend_title, legend_labels)
     plt.savefig(f"{IMAGES_PATH}/{file_path}.pdf")
+
+def format_column_sci(values: pd.Series) -> pd.Series:
+    return values.apply(lambda x: "{:.3e}".format(x))
+
+def save_table_benchmark(
+    file_path: str,
+    df: pd.DataFrame,
+    actions: List[str], 
+    xlabel: str, 
+    ylabel: str, 
+    legend_title: str, 
+    legend_labels: List[str]
+):
+    actions_df = get_actions(df, *actions)
+    headers = ["Amount", "Action", "Mean", "Stddev"]
+    actions_df["Mean"] = format_column_sci(actions_df["Mean"])
+    actions_df["Stddev"] = format_column_sci(actions_df["Stddev"])
+    print(actions_df)
+    table = tabulate(actions_df[headers], headers=headers, tablefmt="latex_raw", showindex=False, disable_numparse=True)
+    f = open(f"{TABLES_PATH}/{file_path}.tex", "w")
+    f.write(table)
+
 
 if __name__ == "__main__":
     benchmarks_df = pd.read_csv('./data/benchmarks.csv')
 
     split_df = split_amount_into_column(benchmarks_df)
-
-    split_df
-    print(split_df)
 
     save_plot_benchmark(
         "plot_generate_result_benchmark",
@@ -70,7 +89,17 @@ if __name__ == "__main__":
         xlabel="The Amount of Nodes (2n + 1)",
         ylabel="The Mean Execution Time in Seconds",
         legend_title="Compute Result",
-        legend_labels=["Only Sum", "Sum and Map", "Sum and Map with Map"]
+        legend_labels=["cataSum", "cataMerkleTree", "cataMerkleTreeWithMap"]
+    )
+
+    save_table_benchmark(
+        "plot_generate_result_benchmark",
+        split_df,
+        ["Generate Result", "Generate (Result, Map)", "Generate (Result, Map) with Map"],
+        xlabel="The Amount of Nodes (2n + 1)",
+        ylabel="The Mean Execution Time in Seconds",
+        legend_title="Compute Result",
+        legend_labels=["cataSum", "cataMerkleTree", "cataMerkleTreeWithMap"]
     )
 
     # show_plot_benchmark(
